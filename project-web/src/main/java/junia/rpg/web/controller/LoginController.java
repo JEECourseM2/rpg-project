71b30ca109773f841d1d2b1275cf8c4a4f857b07
RemiArbache <32673658+RemiArbache@users.noreply.github.com>
2022-02-14 08:41:14 +0100

    Add login controller

---
+++ project-web/src/main/java/junia/rpg/web/controller/LoginController.java
@@ -0,0 +1,19 @@
+package junia.rpg.web.controller;
+
+import junia.rpg.core.service.PartyService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.inject.Inject;
+
+@Controller
+@RequestMapping("/web")
+public class LoginController {
+
+    @GetMapping(value = "/login")
+    public String getListOfParties(ModelMap model) {
+        return "loginForm";
+    }
+}
