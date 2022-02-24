a2cbfbc9be76d5813947677432ac2321ab45d256
RemiArbache <32673658+RemiArbache@users.noreply.github.com>
2022-02-24 11:51:33 +0100

    Finalize profile page

--- project-web/src/main/java/junia/rpg/web/controller/ProfileController.java
+++ project-web/src/main/java/junia/rpg/web/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package junia.rpg.web.controller;
 
 import junia.rpg.core.entity.User;
+import junia.rpg.core.service.PartyService;
 import junia.rpg.core.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,15 +18,26 @@ import javax.servlet.http.HttpSession;
 public class ProfileController {
 
     private UserService userService;
+    private PartyService partyService;
 
     @Inject
-    public ProfileController(UserService userService) {
+    public ProfileController(UserService userService, PartyService partyService) {
         this.userService = userService;
+        this.partyService = partyService;
     }
 
     @GetMapping("profile")
     public String getProfile(HttpSession httpSession, Model model){
         User user = (User) httpSession.getAttribute("user");
+
+        int numberOfPartiesAsPlayer = partyService.findPartiesById(user.getId()).size();
+        int numberOfPartiesAsGM = partyService.findPartiesByGmUserId(user.getId()).size();
+        int totalNumberOfParties = numberOfPartiesAsGM + numberOfPartiesAsGM;
+
+        model.addAttribute("numberOfPartiesAsPlayer", numberOfPartiesAsPlayer);
+        model.addAttribute("numberOfPartiesAsGM", numberOfPartiesAsGM);
+        model.addAttribute("totalNumberOfParties", totalNumberOfParties);
+
         model.addAttribute("currentUser", user);
         model.addAttribute("passwordChangedMessage", " ");
         return "profile";
@@ -36,16 +48,29 @@ public class ProfileController {
                                  @ModelAttribute("currentPassword") String currentPassword,
                                  @ModelAttribute("newPassword") String newPassword,
                                  Model model){
+
+        // Clear passwords from model
+        model.addAttribute("currentPassword", "");
+        model.addAttribute("newPassword", "");
+
         User user = (User) httpSession.getAttribute("user");
         if(currentPassword.equals(user.getPassword())){
             user.setPassword(newPassword);
             userService.save(user);
             model.addAttribute("passwordChangedMessage", "Password successfully changed!");
-            return "profile";
+        }
+        else{
+            model.addAttribute("passwordChangedMessage", "Wrong password");
         }
 
-        model.addAttribute("passwordChangedMessage", "Wrong password");
+        model.addAttribute("currentUser", user);
         return "profile";
+    }
+
+    @PostMapping("logOut")
+    public String logOut(HttpSession httpSession){
+        httpSession.removeAttribute("user");
+        return "redirect:login";
     }
 
 }
